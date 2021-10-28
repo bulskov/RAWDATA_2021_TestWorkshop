1f797b63ac282cacbad7a817bedf8314d7bbf8eb
Henrik Bulskov <bulskov@ruc.dk>
2021-10-28 23:42:29 +0200

    Configure CategoriesController

--- WebService/Controllers/CategoriesController.cs
+++ WebService/Controllers/CategoriesController.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace WebService.Controllers
 {
-    public class CategoriesController
+    [ApiController]
+    [Route("api/categories")]
+    public class CategoriesController : Controller
     {
         
     }
