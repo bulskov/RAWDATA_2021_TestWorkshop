8fc10f547f2300117bc03a242baa914101d29362
Henrik Bulskov <bulskov@ruc.dk>
2021-10-30 23:38:17 +0200

    Create CategoryProfile

---
+++ WebService/ViewModels/Profiles/CategoryProfile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace WebService.ViewModels.Profiles
+{
+    public class CategoryProfile : Profile
+    {
+        
+    }
+}
