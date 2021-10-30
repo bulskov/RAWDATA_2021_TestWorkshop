bf58bb44d07f98d4ddc044da70a39a76f1453605
Henrik Bulskov <bulskov@ruc.dk>
2021-10-30 23:40:01 +0200

    Setup mapping from Category to CategoryViewModel

--- WebService/ViewModels/Profiles/CategoryProfile.cs
+++ WebService/ViewModels/Profiles/CategoryProfile.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using DataServiceLib;
 
 namespace WebService.ViewModels.Profiles
 {
     public class CategoryProfile : Profile
     {
-        
+        public CategoryProfile()
+        {
+            CreateMap<Category, CategoryViewModel>();
+        }
     }
 }
